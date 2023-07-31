3ecae021e4b3b0779e62b1ef481baad713c4a9cd
Johnny Gérard <john@jgerard.dev>
2023-07-31 10:16:36 +0200

    feat: initial routing config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,30 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+@Component({
+  selector: 'app-dummy',
+  standalone: true,
+  template: '<p>dummy works!</p>'
+})
+class DummyComponent {} // Temporary placeholder component
+class DummyGuard {} // Temporary placeholder guard
+
+export const routes: Routes = [
+  { path: 'login', component: DummyComponent },
+  { path: 'new-account', component: DummyComponent },
+  { path: 'user/:id', component: DummyComponent, data: { isPreview: false } },
+
+  // Guarded routes
+  // AuthGuard redirects to 'login' if the user is logged out. Otherwise, it allows the user to
+  // access the requested route.
+  { path: 'links', component: DummyComponent, canActivate: [DummyGuard] },
+  { path: 'profile-details', component: DummyComponent, canActivate: [DummyGuard] },
+  {
+    path: 'preview',
+    component: DummyComponent,
+    canActivate: [DummyGuard],
+    data: { isPreview: true }
+  },
+
+  { path: '**', redirectTo: 'links' }
+];
